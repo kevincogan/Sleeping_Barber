b2d25612dd84b5d4634872de51212f0980afa2ed
Kevin Cogan <kevin.cogan3@mail.dcu.ie>
2020-04-05 16:31:33 +0100

    Fully finished barber question

--- barber.py
+++ barber.py
@@ -10,11 +10,11 @@ def Barber(name, event):
     print("%s starting job..." % (name,))
     while True:
         if len(waiting_room) > 0:
-            waiting_room.pop(0)
-            print("%s is cutting hair..." %(name,))
+            cust = waiting_room.pop(0)
+            print("%s is cutting hair of %s..." %(name, cust,))
             customerInterval = random.randrange(15,25)
             time.sleep(customerInterval)
-            print("%s: All done now..."%(name,))
+            print("%s: All done now with %s..."%(name, cust,))
 
         print("%s checks waiting room..."%(name,))
         if len(waiting_room) == 0:
@@ -24,24 +24,29 @@ def Barber(name, event):
             print("%s awakens!..." % (name,))
 
 def Customer(event):
+    customer_today = 1
     while True:
         print("Customer enters...")
         print("-----------------------------------------------")
         if len(sleeping_barber) > 0:
-            print("Customer wakes barber...")
+            print("Customer " + str(customer_today) + " wakes barber...")
             WakeUp(sleeping_barber[0])
-            waiting_room.append("Customer")
+            waiting_room.append("Customer " + str(customer_today))
 
-        elif len(waiting_room) < 15:
-            print("Customer takes a seat in the waiting room...")
+        elif len(waiting_room) < number_of_chairs:
+            print("Customer " + str(customer_today) + " takes a seat in the waiting room...")
             print("Seats taken is: " + str(len(waiting_room) + 1))
-            waiting_room.append("Customer")
+            waiting_room.append("Customer " + str(customer_today))
 
         else:
             print("Customer leaves the barber shop...")
 
+        if customer_today >= customer_booking:
+            break
+
         customerInterval = random.randrange(5,15)
         time.sleep(customerInterval)
+        customer_today = customer_today + 1
 
 def WakeUp(event):
     sleeping_barber[0].set()
@@ -50,23 +55,21 @@ def WakeUp(event):
 def Block(name, event):
     print("%s goes to sleep..." % (name,))
     event.wait()
-    
 
 def Reset(event):
     event.clear()
 
-
-
-e1 = threading.Event()
-e2 = threading.Event()
-e3 = threading.Event()
-threading.Thread(target=Barber, args=('Barber 1', e1)).start()
-threading.Thread(target=Barber, args=('Barber 2', e2)).start()
-threading.Thread(target=Barber, args=('Barber 3', e3)).start()
-
+number_of_barbers = 4
+number_of_chairs = 15
+customer_booking = 3
+i = 1
+while i != number_of_barbers + 1:
+    barber_name = "Barber " + str(i)
+    threading.Thread(target=Barber, args=(barber_name, threading.Event())).start()
+    i = i + 1
 
 time.sleep(1)
-Customer(e1)
+Customer(threading.Event())
 
 
 
